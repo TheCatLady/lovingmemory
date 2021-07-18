a1b324c605d468d7db872c63ce5e91e04a5b4bd9
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-07-18 10:40:12 -0400

    minor edit

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details">
       <h1 class="title">Order of Service</h1>
-      <p><em>Tuesday, July 27, 2021</em></p>
+      <p>The service will be held the morning of <em>Tuesday, July 27, 2021</em> (US East Coast time).</p>
       <p class="highlight">The family is still making arrangements for the service. This page will be updated with details as soon as they are available.</p>
     </div>
   </div>
