5dc349eea1e5eb07b41cbd609ee99bbdcb9e6ac0
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-07-22 00:20:55 -0400

    add obituary

---
+++ pages/obituary.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section details">
+      <h1 class="title">Obituary 生平</h1>
+      <p>Chin-Yu Lin was born in Taipei, Taiwan on November 23, 1955.  She had two younger brothers.<br/>林錦玉女士於1955年十一月二十三日出生於台灣台北， 她有兩個弟弟。</p>
+      <p>She first met her husband, Huo-Shu Houng, in 1976 at National Taiwan University in Taipei, and got married in 1988 after completing her Ph.D in Physical Chemistry at the Catholic University of America and reuniting with Huo-Shu in Washington, D.C.  Their first daughter, Doris, was born in 1990, and their second daughter, Peggy, followed in 1992.<br/>她與洪火樹先生相識於1976年台灣大學，1988在她完成物理化學博士之後再度相遇於美國華府然後結婚。大女兒洪瑩真於1990誕生，1992接著二女兒洪佩真出生。</p>
+      <p>Chin-Yu worked on combustion kinetics at the National Institute of Standards and Technology (NIST) while she raised her two daughters, later leaving behind a promising career to become a stay-at-home mother.  She had extremely high expectations for her two daughters, both of whom went on to excel in their academic and career pursuits.<br/>錦玉在美國國家標準局做有關燃燒動力學方面的研究，還要負教養兩個女兒的責任。所以她最後放棄自己的工作成為全職的媽媽。她對兩個小孩學業成績有非常高的要求，讓她們日後在自己專業的領域有非常好的表現。</p>
+      <p>Her favorite pastimes included cooking, Sudoku, clipping coupons, and Taiwanese soap operas. But nothing brought her more joy than spending time with her family and loved ones.<br/>她喜歡的業餘活動包括烹飪，尋找日常生活用品的減價優惠券，還有台灣電視肥皂劇。但是沒有任何事可以取代她與家人相處的快樂時光。</p>
+      <p>Chin-Yu leaves behind her husband, two daughters, son-in-law, brother, countless loved ones, and happy memories to be cherished forever.<br/>錦玉留下她的先生、女兒、女婿、弟弟、無數所有她關心喜愛的人及那些值得永遠珍惜的回憶。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
