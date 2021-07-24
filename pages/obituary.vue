75de0753632e4b02a326e8551436471812327448
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-07-24 14:56:44 -0400

    add tribute video

--- pages/obituary.vue
+++ pages/obituary.vue
@@ -10,6 +10,13 @@
       <p>Chin-Yu worked on combustion kinetics at the National Institute of Standards and Technology (NIST) while she raised her two daughters, later leaving behind a promising career to become a stay-at-home mother.  She had extremely high expectations for her two daughters, both of whom went on to excel in their academic and career pursuits.<br/>錦玉在美國國家標準局做有關燃燒動力學方面的研究，還要負教養兩個女兒的責任。所以她最後放棄自己的工作成為全職的媽媽。她對兩個小孩學業成績有非常高的要求，讓她們日後在自己專業領域有非常傑出的表現。</p>
       <p>Her favorite pastimes included cooking, Sudoku, clipping coupons, and Taiwanese soap operas. But nothing brought her more joy than spending time with her family and loved ones.<br/>她喜歡的業餘活動包括烹飪、解數獨謎題、尋找日常生活用品的減價優惠券，還有台灣電視肥皂劇。但是沒有任何事可以取代她與家人相處的快樂時光。</p>
       <p>Chin-Yu leaves behind her husband, two daughters, son-in-law, brother, countless loved ones, and happy memories to be cherished forever.<br/>錦玉留下她的先生、女兒、女婿、弟弟、無數所有她關心喜愛的人及那些值得永遠珍惜的回憶。</p>
+      <p/>
+      <amp-video layout="responsive" controls rotate-to-fullscreen controlslist="nodownload" class="i-amphtml-layout-responsive i-amphtml-layout-size-defined" i-amphtml-layout="responsive">
+        <video width=100% height="auto" playsinline webkit-playsinline controls controlslist="nodownload">
+          <source src="https://ddgfqeo643onm.cloudfront.net/cmi/7/1/6/3/10273617/m_181455.mp4" type="video/mp4"/>
+          <source src="https://d5nffgciuchtn.cloudfront.net/as/assets-mem-com/cmi/7/1/6/3/10273617/m_181455.mp4/-/chin-yu-lin-silver-spring-md-tribute.mp4" type="video/mp4"/>
+        </video>
+      </amp-video>
     </div>
   </div>
 </template>
